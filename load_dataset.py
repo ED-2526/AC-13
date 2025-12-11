24930eac6af7ec2c1bbef191db195c02a23cf011
Vita2024 <164055773+Vita2024@users.noreply.github.com>
2025-12-11 12:02:54 +0100

    Update load_dataset.py

--- load_dataset.py
+++ load_dataset.py
@@ -1,21 +1,10 @@
-# Script para cargar y analizar el dataset
 import pandas as pd
 import os
 
-# --- Función para cargar el dataset ---
+
+# FUNCION PARA CARGAR EL DATASET
 def load_dataset(path):
-    if not os.path.exists(path):
-        raise FileNotFoundError(f"No se encontró el archivo: {path}")
-
-<<<<<<< Updated upstream
-#FUNCION PARWA CARGAR EL DATASET
-def load_dataset():
-    # cambiar con la ruta donde tenemos el csv
-    path = r"Twitter_Data.csv"
-=======
     print(f"Cargando dataset desde: {path}")
->>>>>>> Stashed changes
-
     try:
         df = pd.read_csv(path, encoding="utf-8", header=None)
     except UnicodeDecodeError:
@@ -57,6 +46,7 @@ if __name__ == "__main__":
     path = r"C:\Users\simpl\Desktop\UNIVERSIDAD\cuarto\Aprenentatge Computacional (1er Semestre)\AC Projecte\2526\twitter_training.csv\twitter_training.csv"
     df = load_dataset(path)
     analyze_dataset(df)
+
 
 
 
